c64ff530d2c0bfbe02417a3fcca53a5600b2811b
rayhan <rayhan.rhssoe2@gmail.com>
2023-12-14 10:03:57 +0700

    swr 0, prerender.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   start: {
     server: {
-      preset: "vercel_edge"
+      preset: "vercel_edge",
+      routeRules: {
+        '/**': { swr: 0 }
+      },
+      prerender: {
+        crawlLinks: true
+      }
     }
   }
 });
