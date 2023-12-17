041a6b22a609320faa2c3a645e23d1167a1c6183
rayhan <rayhan.rhssoe2@gmail.com>
2023-12-17 18:43:35 +0700

    vercel edge.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   start: {
     server: {
+      preset: "vercel_edge",
       routeRules: {
         '/about': { swr: 120 },
       },
