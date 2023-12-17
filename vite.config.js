7a645120e73ae443e27215d955366d33d3a24945
rayhan <rayhan.rhssoe2@gmail.com>
2023-12-17 21:14:15 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
       preset: "vercel",
       routeRules: {
         '/about': { isr: true },
+        '/about': { headers: {
+          'Cache-Control': 'max-age=age=31536, stale-while-revalidate',
+          'CDN-Cache-Control': 'max-age=age=31536, stale-while-revalidate',
+          'Vercel-CDN-Cache-Control': 'max-age=age=31536, stale-while-revalidate',
+        } }
       },
       prerender: {
         routes: ["/about"]
